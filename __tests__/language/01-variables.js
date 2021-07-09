386e19b6dd68e9771a7fdc2513e4ef24761e01e4
Bertrand <bertrand.tarascon@epita.fr>
2021-07-09 18:46:00 +0200

    fix(01): Variables - 4 var I goes through 1 loop. let J is initialized in the loop block only, so J stay undefined.

--- __tests__/language/01-variables.js
+++ __tests__/language/01-variables.js
@@ -2,7 +2,6 @@ describe("Variables", () => {
   nom = "Pierre";
 
   it("Variables - 1", () => {
-    // _
     expect(nom).toBe("Thierry");
     expect(window.nom).toBe("Thierry");
   });
@@ -10,7 +9,6 @@ describe("Variables", () => {
   age = 6;
 
   it("Variables - 2", () => {
-    // _
     expect(age).toBe(4);
     expect(window.age).toBe(6);
   });
@@ -24,9 +22,9 @@ describe("Variables", () => {
     var i, j;
 
     for (var i = 0; i < 1; i++) {}
-    expect(i).toBe(_);
+    expect(i).toBe(1);
 
     for (let j = 0; j < 1; j++) {}
-    expect(j).toBe(_);
+    expect(j).toBe(undefined);
   });
 });
