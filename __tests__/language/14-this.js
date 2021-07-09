62084f353c4153f80429edcfe5a8f4f7f03dc02c
Alexandre James <alexandre.james@epita.fr>
2021-07-09 20:13:17 +0200

    fixed tests on this (1: added this.name)

--- __tests__/language/14-this.js
+++ __tests__/language/14-this.js
@@ -3,7 +3,7 @@ describe("This", () => {
     var person = {
       name: "bob",
       intro() {
-        return "Hello, my name is " + _;
+        return "Hello, my name is " + this.name;
       },
     };
     expect(person.intro()).toBe("Hello, my name is bob");
@@ -21,7 +21,7 @@ describe("This", () => {
 
     window.name = "Peter";
 
-    expect(_).toBe("Hello, my name is Peter");
+    expect().toBe("Hello, my name is Peter");
   });
 
   it("This - 3", () => {
